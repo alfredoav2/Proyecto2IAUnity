2ef3a8b3da81703fadfeed58247c77c9a563e697
alfredoav2 <alfredo.almazan@saei.mx>
2020-08-26 20:17:11 -0500

    grafo y nodografo

--- Proyecto2IAGrupalBusquedaNoInformada/Assets/Scripts/BSNode.cs
+++ Proyecto2IAGrupalBusquedaNoInformada/Assets/Scripts/BSNode.cs
@@ -1,7 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public struct BSNode <T>
+public class BSNode <T>
 {
-
+    T data;
+    public int val = 100000;
+    public bool visitado = false;
+    public BSNode<T> padre;
+    public List<BSNode<T>> adyacentes;
+    public List<int> costoAdyacentes;
+    public T getData()
+    {
+        return data;
+    }
+    public BSNode() { }
+    public BSNode(T d, int v)
+    {
+        data = d;
+        val = v;
+    }
+    ~BSNode() { }
 }
